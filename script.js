5a95810ac4dbc94d6d1a98d411a27674b144851a
russhenson <49965783+russhenson@users.noreply.github.com>
2020-11-25 23:15:27 +0800

    linksdash

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+// getAboutMe - renders data from firebase to the textarea box
 function getAboutMe (description) {
     var aboutMeDesc = get
 }
+
+/*
+saveAboutMe
+- check if about-me data was changed
+- if yes:
+    - unable save button
+    - place data in the firebase
+    - render the new data in the textareabox (getAboutMe func)
+
+- if no:
+    - disable save button
+
+*/
+
